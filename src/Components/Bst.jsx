4adeecc286633c679eaf2ba01d3ac81e7c997cde
waleery <walenda1997@gmail.com>
2023-11-10 20:22:10 +0100

    Fixed problem with path and organized code

--- src/Components/Bst.jsx
+++ src/Components/Bst.jsx
@@ -3,10 +3,12 @@ import Tree from "react-d3-tree";
 import initialBstData from "../data/initialBstData.json";
 import { NavbarContext } from "../App";
 import { markLiksToNodesWithoutChildern, markSearchedValue, shakeElement } from "./Helpers/ReactD3Helpers";
+import { balanceBST, findValue, insertNode } from "./Helpers/BstHelpers";
 
 const Bst = () => {
     const [bstData, setBstData] = useState(initialBstData);
     const [bstDataWithoutPath, setBstDataWithouPath] = useState();
+    const [shouldFindPath, setShouldFindPath] = useState(false)
     const [searchedValue, setSearchedValue] = useState()
 
     const [translate, setTranslate] = useState();
@@ -71,6 +73,7 @@ const Bst = () => {
         const updatedBstData = { name: randomValue, path: false, children: [{}, {}] };
         setBstData(updatedBstData);
         setBstDataWithouPath();
+        setSearchedValue()
     };
 
     const handleFindValue = (valueToFind) => {
@@ -84,119 +87,35 @@ const Bst = () => {
         if(isValue){
             if(!searchedValue){
                 setBstDataWithouPath(JSON.parse(JSON.stringify(bstData)))
+                setShouldFindPath(false)
             }
             setBstData(bstDataCopy);
             setSearchedValue(valueToFind)
+            setShouldFindPath(false)
         } else {
             shakeElement("tree")
         }
     }
 
-    const findValue = (valueToFind, root = {...bstData}) => {
-        if(root.name === null || Object.keys(root).length === 0){
-            return false
-        }
-
-        if(root.name === valueToFind){
-            root.path = true; // Mark the node as part of the path
-            root.searchedValue = true
-            return true;
-        }
-
-        if( valueToFind > root.name){
-            root.path = true
-            return findValue(valueToFind, root.children[1])
-        } else {
-            root.path = true
-            return findValue(valueToFind, root.children[0])
-        }
-    }
 
     useEffect(() => {
-        if(searchedValue){
+        if(shouldFindPath){
             handleFindValue(searchedValue)
         }
     }, [bstData]);
 
     const handleBalance = () => {
         setBstData(balanceBST({...bstData}))
+        setShouldFindPath(true)
 
         if(searchedValue){
             setBstDataWithouPath(balanceBST({...bstDataWithoutPath}))
         }
     }
 
-    function balanceBST(bst) {
-        
-        const values = inOrderTraversal(bst);
-        
-        console.log(values)
-        // Posortuj tablicę
-        values.sort((a, b) => a - b);
     
-        console.log(values)
 
-        const balanced = balanceBstFromArray(values)
-        console.log(balanced)
-
-        return balanced
-    }
     
-    function inOrderTraversal(root) {
-        const values = [];
-        
-        function traverse(node) {
-            if (node) {
-                if(Object.keys(node.children[0]).length !== 0)
-                    traverse(node.children[0]);
-
-                values.push(node.name);
-
-                if(Object.keys(node.children[1]).length !== 0)
-                    traverse(node.children[1]);
-            }
-        }
-        traverse(root);
-        return values;
-    }
-    
-    const balanceBstFromArray = (sortedValues) => {
-        if(sortedValues.length === 0){
-            return {}
-        }
-
-        const middleIndex = Math.floor(sortedValues.length / 2)
-        const rootValue = sortedValues[middleIndex]
-        const leftValues = sortedValues.slice(0, middleIndex)
-        const rightValues = sortedValues.slice(middleIndex+1)
-        
-        const root = {
-            name: rootValue,
-            children: [
-                balanceBstFromArray(leftValues),
-                balanceBstFromArray(rightValues)
-            ]
-        }
-        
-        return root
-    }
-
-    const insertNode = (root, valueToInsert) => {
-        if (!root || Object.keys(root).length === 0) {
-            return (root = { name: valueToInsert,path: false, children: [{}, {}] });
-        }
-
-        if (root.name == valueToInsert) {
-            console.log("Liczba jest juz w drzewie");
-        }
-
-        if (valueToInsert < root.name)
-            root.children[0] = insertNode(root.children[0], valueToInsert);
-        else if (valueToInsert > root.name)
-            root.children[1] = insertNode(root.children[1], valueToInsert);
-
-        return root;
-    };
 
     const handleInsertValue = (valueToInsert) => {
         if(!valueToInsert){
